e36f75ca40c8014d0ea8928e4d0f0a2a7fb0d8a5
SPMY3 <spmythri5@gmail.com>
2025-10-07 12:54:50 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -3,102 +3,178 @@
 #include <string.h>
 
 #define MAX 100
+
 // Remove spaces from the expression so we can parse it easily
-void removeSpaces(char *s) {
-    int i = 0, j = 0;
-    while (s[i]) { 
-        if (s[i] != ' ') s[j++] = s[i];  
-        i++; 
+void removeSpaces(char *expression)
+{
+    int i = 0;
+    int j = 0;
+
+    while (expression[i])
+    {
+        if (expression[i] != ' ')
+        {
+            expression[j++] = expression[i];
+        }
+        i++;
     }
-    s[j] = '\0'; 
+    expression[j] = '\0';
 }
 
 // Check that the expression only has digits and valid operators
-int valid(char *s) {
-    int len = strlen(s);
-    if (len == 0) return 0;
-    if (!isdigit(s[0]) && s[0] != '-') return 0;
-    if (!isdigit(s[len - 1])) return 0;
-    for (int i = 1; i < len; i++) {
-        if (!isdigit(s[i]) && s[i] != '+' && s[i] != '-' && s[i] != '*' && s[i] != '/')
+int isValidExpression(const char *expression)
+{
+    int length = strlen(expression);
+
+    if (length == 0)
+    {
+        return 0;
+    }
+
+    if (!isdigit(expression[0]) && expression[0] != '-')
+    {
+        return 0;
+    }
+
+    if (!isdigit(expression[length - 1]))
+    {
+        return 0;
+    }
+
+    for (int i = 1; i < length; i++)
+    {
+        if (!isdigit(expression[i]) &&
+            expression[i] != '+' &&
+            expression[i] != '-' &&
+            expression[i] != '*' &&
+            expression[i] != '/')
+        {
             return 0;
-        if (!isdigit(s[i]) && !isdigit(s[i-1]))
+        }
+
+        if (!isdigit(expression[i]) && !isdigit(expression[i - 1]))
+        {
             return 0;
+        }
     }
     return 1;
 }
 
-// Split the input into numbers and operators
-int parse(char *s, int nums[], char ops[]) {
-    int ni = 0, oi = 0, i = 0;
-    while (s[i]) {
-        if (isdigit(s[i])) {
-            int n = 0;
-            while (isdigit(s[i])) {
-                n = n * 10 + (s[i] - '0');
-                i++; 
+// Split the input into numbers and operators array
+int parseExpression(const char *expression, int numbers[], char operators[])
+{
+    int numberIndex = 0;
+    int operatorIndex = 0;
+    int i = 0;
+
+    while (expression[i])
+    {
+        if (isdigit(expression[i]))
+        {
+            int value = 0;
+            while (isdigit(expression[i]))
+            {
+                value = value * 10 + (expression[i] - '0');
+                i++;
             }
-            nums[ni++] = n;
-        } else {
-            ops[oi++] = s[i++];
+            numbers[numberIndex++] = value;
+        }
+        else
+        {
+            operators[operatorIndex++] = expression[i++];
         }
     }
-    ops[oi] = '\0';
-    return ni;
+
+    operators[operatorIndex] = '\0';
+    return numberIndex;
 }
 
-// Handle * and / first before + and - 
-int mulDiv(int nums[], char ops[], int n, int *newN) {
-    int tempN[MAX], tempO[MAX], ni = 0, oi = 0;
-    tempN[ni++] = nums[0];
+// Handle * and / first before + and -
+int handleMulDiv(int numbers[], char operators[], int count, int *updatedCount)
+{
+    int tempNumbers[MAX];
+    char tempOperators[MAX];
+    int numberIndex = 0;
+    int operatorIndex = 0;
 
-    for (int i = 0; ops[i]; i++) {
-        if (ops[i] == '*') {
-            tempN[ni - 1] *= nums[i + 1];
-        } else if (ops[i] == '/') {
-            if (nums[i + 1] == 0) { 
+    tempNumbers[numberIndex++] = numbers[0];
+
+    for (int i = 0; operators[i]; i++)
+    {
+        if (operators[i] == '*')
+        {
+            tempNumbers[numberIndex - 1] *= numbers[i + 1];
+        }
+        else if (operators[i] == '/')
+        {
+            if (numbers[i + 1] == 0)
+            {
                 printf("Error: Division by zero\n");
-                return 0; 
+                return 0;
             }
-            tempN[ni - 1] /= nums[i + 1];  
-        } else {
-            tempO[oi++] = ops[i];
-            tempN[ni++] = nums[i + 1];
+            tempNumbers[numberIndex - 1] /= numbers[i + 1];
+        }
+        else
+        {
+            tempOperators[operatorIndex++] = operators[i];
+            tempNumbers[numberIndex++] = numbers[i + 1];
         }
     }
 
-    tempO[oi] = '\0';
-    for (int i = 0; i < ni; i++) nums[i] = tempN[i];
-    for (int i = 0; i < oi; i++) ops[i] = tempO[i];
-    ops[oi] = '\0';
-    *newN = ni;
+    tempOperators[operatorIndex] = '\0';
+
+    for (int i = 0; i < numberIndex; i++)
+    {
+        numbers[i] = tempNumbers[i];
+    }
+
+    for (int i = 0; i < operatorIndex; i++)
+    {
+        operators[i] = tempOperators[i];
+    }
+
+    operators[operatorIndex] = '\0';
+    *updatedCount = numberIndex;
     return 1;
 }
 
 // handle + and - using the remaining numbers and operators
-int addSub(int nums[], char ops[], int n) {
-    int res = nums[0];
-    for (int i = 0; ops[i]; i++) 
-        res = (ops[i] == '+') ? res + nums[i + 1] : res - nums[i + 1]; 
-    return res;
+int handleAddSub(const int numbers[], const char operators[], int count)
+{
+    int result = numbers[0];
+    for (int i = 0; operators[i]; i++)
+    {
+        result = (operators[i] == '+') ? result + numbers[i + 1] : result - numbers[i + 1];
+    }
+    return result;
 }
 
-int main() {
-    char expr[MAX];   
-    int nums[MAX], newN; 
-    char ops[MAX];
+int main(void)
+{
+    char expression[MAX];
+    int numbers[MAX];
+    int updatedCount;
+    char operators[MAX];
+
     printf("Enter expression: ");
-    fgets(expr, MAX, stdin); 
-    expr[strcspn(expr, "\n")] = '\0';
-    removeSpaces(expr); 
-    if (!valid(expr)) {
+    fgets(expression, MAX, stdin);
+    expression[strcspn(expression, "\n")] = '\0';
+
+    removeSpaces(expression);
+
+    if (!isValidExpression(expression))
+    {
         printf("Error: Invalid expression\n");
         return 0;
     }
-    int n = parse(expr, nums, ops);
-    if (!mulDiv(nums, ops, n, &newN)) 
+
+    int count = parseExpression(expression, numbers, operators);
+
+    if (!handleMulDiv(numbers, operators, count, &updatedCount))
+    {
         return 0;
-    printf("%d\n", addSub(nums, ops, newN));
+    }
+
+    printf("%d\n", handleAddSub(numbers, operators, updatedCount));
     return 0;
 }
-
